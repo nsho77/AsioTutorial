d959aa1d8534831e40ba58a125e4f9f2385d94c6
shroh <nsho77@naver.com>
2022-05-02 22:45:22 +0900

    boost asio#2
    - Timer.5 - Synchronising completion handlers in multithreaded programs

--- AsioTutorial/Main.cpp
+++ AsioTutorial/Main.cpp
@@ -1,12 +1,76 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind/bind.hpp>
+
+#pragma comment(lib, "boost_thread-vc140-mt-gd.lib")
+
+class Printer
+{
+public:
+	Printer(boost::asio::io_context& io)
+		: strand_(boost::asio::make_strand(io)),
+		  timer1_(io, boost::asio::chrono::seconds(1)),
+		  timer2_(io, boost::asio::chrono::seconds(1)),
+		  count_(0)
+	{
+		timer1_.async_wait(boost::asio::bind_executor(strand_,
+			boost::bind(&Printer::print1, this)));
+
+		timer2_.async_wait(boost::asio::bind_executor(strand_,
+			boost::bind(&Printer::print2, this)));
+	}
+
+	~Printer()
+	{
+		std::cout << "Final count is " << count_ << std::endl;
+	}
+	void print1()
+	{
+		if (count_ < 100)
+		{
+			std::cout << "Timer 1: " << count_ << std::endl;
+			++count_;
+
+			timer1_.expires_at(timer1_.expiry() + boost::asio::chrono::seconds(1));
+
+			timer1_.async_wait(boost::asio::bind_executor(strand_,
+				boost::bind(&Printer::print1, this)));
+			/*timer1_.async_wait(
+				boost::bind(&Printer::print1, this));*/
+		}
+	}
+
+	void print2()
+	{
+		if (count_ < 100)
+		{
+			std::cout << "Timer 2: " << count_ << std::endl;
+			++count_;
+
+			timer2_.expires_at(timer2_.expiry() + boost::asio::chrono::seconds(1));
+
+			timer2_.async_wait(boost::asio::bind_executor(strand_,
+				boost::bind(&Printer::print2, this)));
+			/*timer2_.async_wait(
+				boost::bind(&Printer::print2, this));*/
+		}
+	}
+
+private:
+	boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+	boost::asio::steady_timer timer1_;
+	boost::asio::steady_timer timer2_;
+	int count_;
+};
 
 int main()
 {
 	boost::asio::io_context io;
-	boost::asio::steady_timer t(io, std::chrono::seconds(5));
-	t.wait();
+	Printer p(io);
+	boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
+	io.run();
+	t.join();
 
-	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
